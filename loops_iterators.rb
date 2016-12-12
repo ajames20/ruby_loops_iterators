935babc7d88b23d63def4f5c305aa391f35146b2
Andrew James <ajames20@gmail.com>
2016-12-12 11:35:40 +0200

    edits to reccursion exercise

--- loops_iterators.rb
+++ loops_iterators.rb
@@ -53,13 +53,14 @@ Write a method that counts down to zero using recursion.
 def count_down(start)
   puts start
   if start <= 0
-    puts start
+    puts "Done!"
   else
     count_down(start - 1)
   end
 end
 
 count_down(10)
+count_down(30)
 puts "Done!"
 
 
